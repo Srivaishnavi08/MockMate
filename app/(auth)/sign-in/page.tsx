388e918efe2755dd099a54d39865375a037089ca
SriVaishnavi <bellarysrivaishnavi@gmail.com>
2025-04-20 19:43:05 +0530

    after setup of firebase and user authentication

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AuthForm from '../../components/AuthForm'; 
+
+function page() {
+  return (
+    <AuthForm type="sign-in"/>
+  );
+}
+
+export default page;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { dummyInterviews } from '@/constants'
+import InterviewCard from '../components/InterviewCard'
+
+const page = () => {
+  return (
+    <>
+        <section  className="card-cta">
+          
+            <div className="flex flex-col gap-6 max-w-lg">
+              <h2>Get Interview-Ready with AI-powered Practice & Feedback</h2>
+              <p className='text-lg'>Parctice on real interview questions & get instant feedback</p>
+              <button className='btn-primary max-sm:w-full'><Link href="/interview">Start an Interview</Link></button>
+            </div>
+          <Image src='/robot.png' alt='robot' height={400} width={400} className='max-sm:hidden'/>
+        </section>
+
+        <section className='flex flex-col gap-6 mt-8'>
+
+          <h2>Your Interview</h2>
+          <div className='interviews-section'>
+            
+            {dummyInterviews.map((interview)=>(
+              <InterviewCard {...interview} key={interview.id}/>
+            ))}
+          </div>
+        </section>
+
+        <section className='flex flex-col gap-6 mt-8'>
+          <h2>Take an Interview</h2>
+          <div className='interviews-section'>
+            
+            {dummyInterviews.map((interview)=>(
+              <InterviewCard {...interview} key={interview.id}/>
+            ))}
+          </div>
+        </section>
+    </>
+  )
+}
+
+export default page;
